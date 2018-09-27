1411998ff27316fb68caaa4ca09cebf439ef3e7b
samir <samir.benzenine@gmail.com>
2018-09-27 12:44:41 +0100

    Display Current Location weather information for the current day - closes #2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  appId: '79bddd9c88ba6222018830172ecedb6e',
+  baseUrl: 'http://',
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
